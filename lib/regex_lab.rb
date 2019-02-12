b337b360f2d39c09110213edfa216ea1b005f104
Christoph Gruener <christophgruener@me.com>
2019-02-12 21:19:59 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,18 +4,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z]|[!.:,?]$/).length == 2 ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone
 end
