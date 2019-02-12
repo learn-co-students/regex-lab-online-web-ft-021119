b7ba3baa3fa91c9e02fba0cdd1621877d092181d
Joan in DC <joanrigdon@gmail.com>
2019-02-12 06:43:47 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,27 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/\A+[aeiouAEIOU]/)
+    TRUE
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b+un+[a-zA-Z]+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].[\.?!]\b/)
+    TRUE
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
