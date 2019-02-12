9a201748fb1190627ba2e286edc7edd1c83ad2e1
Vinh Do <vinhdo07@gmail.com>
2019-02-12 05:22:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,13 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.downcase.match(/\A^[aeiou]/)? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  un_array = text.scan(/un[a-z]*/)
+  un_array.keep_if {|index| index.end_with?("ing")}
+
 
 end
 
