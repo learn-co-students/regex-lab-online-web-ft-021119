8f008a56d81c997ba3589b9d64993081b549e204
Vinh Do <vinhdo07@gmail.com>
2019-02-12 06:46:33 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,18 +7,16 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   un_array = text.scan(/un[a-z]*/)
   un_array.keep_if {|index| index.end_with?("ing")}
-
-
 end
 
 def words_five_letters_long(text)
-
+  my_array = text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]/) && text.match(/[?.!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  binding.pry
 end
