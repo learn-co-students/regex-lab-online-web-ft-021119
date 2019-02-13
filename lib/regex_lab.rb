7f6867feb2a5446e7543a18aad1f369682951aa6
Lu Daley <daleyluis@gmail.com>
2019-02-13 22:08:47 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,25 +7,24 @@ def starts_with_a_vowel?(word)
 def words_starting_with_un_and_ending_with_ing(text)
 #returns an array with the words starting with
 # 'un' and ending with 'ing'
-
-starts = []
-if text.start_with?("un")
-  starts << text
-ends = text.scan(/\w+ing/)
-starts << ends
-#erase duplicates
-#binding.pry
-#text.scan
+text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+#returns an array of words that are five letters long
+text.scan(/\b\w{5}\b/)
+#binding.pry
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+#Returns true for text starting
+#with a capital letter and ending with puncutation
+text.match(/[A-Z]\W/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  #"2438894546"
+  #returns true for valid phone numbers, regardless of formatting
+phone.match(/(\d *?){10}|(\(\d{3}\)((\d{3}-\d{4})|\d{7})\b)/) ? true : false
+#binding.pry
 end
