6868992c869417579c4c69014784a375a15a7e76
Lu Daley <daleyluis@gmail.com>
2019-02-13 21:15:16 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,21 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-#  binding.pry
-  vowels = %w(a e i o u)
-    word.capaitalize.start_with?(*vowels)
-      return true
-    else
-      false
-    end
-end
+  vowels = %w(A E I O U a e i o u)
+    word.start_with?(*vowels)
+    #binding.pry
+  end
 def words_starting_with_un_and_ending_with_ing(text)
+#returns an array with the words starting with
+# 'un' and ending with 'ing'
 
+starts = []
+if text.start_with?("un")
+  starts << text
+ends = text.scan(/\w+ing/)
+starts << ends
+#erase duplicates
+#binding.pry
+#text.scan
 end
 
 def words_five_letters_long(text)
