a7f4a07809f6ee280a93d34b8b865fcbd94b1944
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-23 04:30:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  output = /\A[aeiouAEIOU]\w/.match(word)
+  return output==nil ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  output = text.match(/^[A-Z].*[.]$/)
+  return output==nil ? false : true
 end
 
 def valid_phone_number?(phone)
-
+  output = phone.scan(/\d/)
+  return output.length==10 ? true : false
 end
